2a71e56bcce8bc11379e9cb744969a991e1e83d3
aghassaei <aghassaei@reed.edu>
2021-12-02 18:01:49 -0800

    pushing

--- climb_chromosome.hh
+++ climb_chromosome.hh
@@ -6,10 +6,10 @@
 
 class ClimbChromosome : public Chromosome {
   public:
+
   ClimbChromosome(const Cities *cities_ptr) : Chromosome(cities_ptr) {}
   void mutate() override;
-
-  Chromosome::Chromosome* clone() override;
+  Chromosome* clone() override;
 
   private:
 
